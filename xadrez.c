6a9e1a90ad14e0c37bd6b195efaebc12e54d371c
gabrieltecnologociber47-del <gabrieltecnologociber47@gmail.com>
2026-06-04 15:59:03 +0000

    Desafio Novato - Usei estruturas de repetição while, do-while, for.

--- xadrez.c
+++ xadrez.c
@@ -6,35 +6,35 @@
 
 int main(){
     
-    int i;
+        int i;
 
-    // Mover a Torre 5 casas para a direita.
-    printf("Torre:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); // Imprime a direção do movimento.
-    }
-        printf("\n");
+            // Mover a Torre 5 casas para a direita.
+                printf("Torre:\n");
+                    for (int i = 0; i < 5; i++) {
+                            printf("Direita\n"); // Imprime a direção do movimento.
+                                }
+                                        printf("\n");
 
-    // Mover o Bispo 5 casas para a diagonal (cima + direita).
-    printf("Bispo:\n");
-     i = 0;
-     while (i < 5) {
-        printf("Cima Direita\n"); // Imprime a direção do movimento.
-        i++;
-     }
-        printf("\n");
-     
-    // Mover a Rainha 8 casas para frente.
-     printf("Rainha:\n");
-     i = 0;
-     do {
-         printf("Esquerda\n"); // Imprime a direção do movimento.
-         i++;
-     } while (i < 8);
-        
-     
-    
-     return 0;
-     
+                                            // Mover o Bispo 5 casas para a diagonal (cima + direita).
+                                                printf("Bispo:\n");
+                                                     i = 0;
+                                                          while (i < 5) {
+                                                                  printf("Cima Direita\n"); // Imprime a direção do movimento.
+                                                                          i++;
+                                                                               }
+                                                                                       printf("\n");
+                                                                                            
+                                                                                                // Mover a Rainha 8 casas para frente.
+                                                                                                     printf("Rainha:\n");
+                                                                                                          i = 0;
+                                                                                                               do {
+                                                                                                                        printf("Esquerda\n"); // Imprime a direção do movimento.
+                                                                                                                                 i++;
+                                                                                                                                      } while (i < 8);
+                                                                                                                                              
+                                                                                                                                                   
+                                                                                                                                                       
+                                                                                                                                                            return 0;
+                                                                                                                                                                 
 
-}
+                                                                                                                                                                 }
